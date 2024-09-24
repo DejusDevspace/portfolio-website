bf7f5bf217dc85bd793d1b83a00a5b387cd54b07
Deju <ojomideju2003@gmail.com>
2024-09-24 20:17:33 +0100

    added typing on homepage and uploaded favicon

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,35 @@
+const text = "Welcome to my Portfolio!";
+
+// The speed of typing (in milliseconds)
+const typingSpeed = 100;
+
+const textElement = document.getElementById("intro-text");
+
+function typeWriterEffect(text, typingSpeed) {
+  let i = 0;
+
+  // Function to display the text
+  function type() {
+    if (i < text.length) {
+      textElement.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(type, typingSpeed); // Typing next letter
+    } else {
+      setTimeout(clearText, 2000); // Pause after full text is written, then clear
+    }
+  }
+
+  // Function to clear the text and restart typing
+  function clearText() {
+    textElement.innerHTML = "";
+    i = 0;
+    setTimeout(type, typingSpeed); // Start typing again
+  }
+
+  type(); // Initial call to start typing
+}
+
+// Call the typewriter effect when the page loads
+window.onload = function () {
+  typeWriterEffect(text, typingSpeed);
+};
