5fbd62f9780f81db83179e2b7094884953fc146a
Deju <ojomideju2003@gmail.com>
2024-10-10 21:15:01 +0100

    :completed home page responsive

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,12 @@
+// Navbar toggling
+const toggleButton = document.getElementById("toggle-button");
+const sidebar = document.querySelector(".sidebar");
+
+toggleButton.addEventListener("click", () => {
+  sidebar.classList.toggle("active"); // Toggle the active class to show/hide the sidebar
+});
+
+// Introduction text display loop
 const text = "Welcome to my Portfolio!";
 
 // The speed of typing (in milliseconds)
